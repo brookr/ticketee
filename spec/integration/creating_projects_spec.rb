72bf5adfbd15b75650a2f5327ca53f49bbdb392f
Brook Riggio <brooke.riggio@gmail.com>
2013-01-29 16:19:22 -0800

    Chapter 3

--- spec/integration/creating_projects_spec.rb
+++ spec/integration/creating_projects_spec.rb
@@ -8,5 +8,18 @@ feature "Creating Projects" do
     fill_in "Description", with: "A Mac OS X text editor"
     click_button "Create Project"
     page.should have_content("Project has been created.")
+
+    project = Project.find_by_name("TextMate 2")
+    page.current_url.should == project_url(project)
+    title = "TextMate 2 - Projects - Ticketee"
+    find("title").should have_content(title)
+  end
+
+  scenario "can not create a project without a name" do
+    visit '/'
+    click_link 'New Project'
+    click_button 'Create Project'
+    page.should have_content("Project has not been created.")
+    page.should have_content("Name can't be blank")
   end
 end
